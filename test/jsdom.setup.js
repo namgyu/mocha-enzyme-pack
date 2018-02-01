15146a6fca5908cc2ad35e398241db415c6e0451
국남규 <3gguild@naver.com>
2018-02-01 10:43:42 +0900

    chore

--- test/jsdom.setup.js
+++ test/jsdom.setup.js
@@ -2,8 +2,7 @@
  * Error: It looks like you called `mount()` without a global document being loaded.
  * https://github.com/airbnb/enzyme#full-dom-rendering
  */
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+import { JSDOM } from 'jsdom';
 const dom = new JSDOM('<!DOCTYPE html><html><head></head><body></body></html>');
 
 global.window = dom.window;
